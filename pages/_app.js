b9bfd38e2e91b528f1732d86182c89bbb6a3efd7
Angie <angie@genosha.com.ar>
2021-08-03 16:29:35 -0300

    - Add eslint, prettier & style-components - Delete unnecessary files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html, body{
+  box-sizing: border-box;
+  margin:0;
+  padding: 0;
+}
+`
+
+const theme = {
+  colors: {
+    primary: '#fafafa',
+  },
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
